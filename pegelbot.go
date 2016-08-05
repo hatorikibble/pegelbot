de944b407560f1012e0bba66319ff70f6a83f410
Peter Mayr <at.peter.mayr@posteo.de>
2016-08-05 21:55:46 +0200

    tweet scheduled slogans, english comments

--- pegelbot.go
+++ pegelbot.go
@@ -52,7 +52,7 @@ var level_history [5]int64
 var variance int64
 var last_tweet_timestamp int64
 
-// init_bot reads the configuration and opens a logger
+// init_bot reads the configuration file and opens a logger
 func init_bot() {
 
 	config_file := os.Getenv("PEGELBOT_CONFIG")
@@ -74,7 +74,9 @@ func init_bot() {
 	logger.Print("Started...")
 }
 
-// API Abfragen
+// get_water_level queries the high water API, saves the value in
+// level_history[] and calculates the variance between the current
+// and the oldest value
 func get_water_level() {
 
 	logger.Print("Querying ", configuration.Pegel_API_URL)
@@ -86,30 +88,31 @@ func get_water_level() {
 	hwpegel := new(Hochwasserpegel)
 	err = xml.Unmarshal(body, hwpegel)
 	check(err)
-	logger.Printf("Pegel ist %s m am %s um %s", hwpegel.Pegel, hwpegel.Datum, hwpegel.Uhrzeit)
-	// Number format
+	logger.Printf("Water mark is %s m on %s at %s", hwpegel.Pegel, hwpegel.Datum, hwpegel.Uhrzeit)
+	// convert string to int
 	pegel_int := m_string_to_cm(hwpegel.Pegel)
 
-	// ablegen, alte Werte wandern nach hinten
+	// save values in level_history, discard oldest
 	for i := 4; i > 0; i-- {
 		level_history[i] = level_history[i-1]
 	}
 	level_history[0] = pegel_int
 
-	// Streuung berechnen, wenn genuegend Werte da sind
+	// calculate variance if there is enough data
 	if level_history[4] > 0 {
 		variance = level_history[4] - level_history[0]
 	} else {
 		variance = 0
 	}
-	// Absoluten Wert verwenden
+	// use absolut value
 	if variance < 0 {
 		variance = -variance
 	}
 
 }
 
-// cm in Stangen Koelsch umrechnen
+// convert_to_koelsch converts the centimeter value in
+// number of Koelsch glasses
 func convert_to_koelsch(conv_number_cm int64) int64 {
 	const koelsch_stange_cm = 15
 	conv_number_koelsch := conv_number_cm / koelsch_stange_cm
@@ -118,13 +121,13 @@ func convert_to_koelsch(conv_number_cm int64) int64 {
 	return conv_number_koelsch
 }
 
-// cm in m umrechnen
+// cm_to_m converts centimeters to meters
 func cm_to_m(cm int64) float64 {
 	m := float64(cm) / 100
 	return m
 }
 
-// Textangabe in Metern in cm Zahlenwert umrechnen
+// m_string_to_cm converts a string meter value to centimeters
 func m_string_to_cm(m_string string) int64 {
 	var cm int64
 	rp := regexp.MustCompile(",")
@@ -138,7 +141,8 @@ func m_string_to_cm(m_string string) int64 {
 	return cm
 }
 
-// passenden Spruch aus Textdateien holen
+// get_tendency_message selects the appropriate slogan
+// to a water tendency
 func get_tendency_message(tendency string) string {
 	var file string
 	var msg []string
@@ -159,7 +163,7 @@ func get_tendency_message(tendency string) string {
 	// read source file
 	content, err := ioutil.ReadFile(file)
 	check(err)
-	//fmt.Print(string(dat))
+
 	msg = strings.Split(string(content), "\n")
 
 	num_msg = len(msg) - 1
@@ -169,6 +173,8 @@ func get_tendency_message(tendency string) string {
 
 }
 
+// find_tendency returs the water level
+// tendency according to a history of values
 func find_tendency() string {
 	var tendency string
 
@@ -184,11 +190,13 @@ func find_tendency() string {
 
 }
 
+// write_tendency_tweet tweets a slogan in koelsch
+// that fits to the water tendency
 func write_tendency_tweet(tendency string) {
 	var slogan string
 	var tweet_msg string
 
-	logger.Print("Kölscher Tendenztweet")
+	logger.Print("tweet slogan and tendency")
 	slogan = get_tendency_message(tendency)
 	if tendency == "up" {
 		tweet_msg = fmt.Sprintf("%s Der Rheinpegel steigt! Derzeit liegen wir bei %.2f m #koeln #rhein", slogan, cm_to_m(level_history[0]))
@@ -201,8 +209,26 @@ func write_tendency_tweet(tendency string) {
 	tweet(tweet_msg)
 }
 
-func write_scheduled_tweet() {
-	logger.Print("Zeit für einen Tweet")
+// write_scheduled_message_tweet tweets the current water mark
+// and an appropriate slogan
+// does not mention the current tendency
+func write_scheduled_message_tweet(tendency string) {
+	var slogan string
+	var tweet_msg string
+
+	logger.Print("tweet slogan")
+	slogan = get_tendency_message(tendency)
+
+	tweet_msg = fmt.Sprintf("\"%s\" Der #Rhein bei #Koeln steht derzeit bei %.2f m", slogan, cm_to_m(level_history[0]))
+
+	logger.Printf("Message has %d characters", len(tweet_msg))
+	tweet(tweet_msg)
+}
+
+// write_scheduled_koelsch_tweet tweets the water mark expressed in
+// number of koelsch glasses
+func write_scheduled_koelsch_tweet() {
+	logger.Print("tweet koelsch glasses")
 	tweet(fmt.Sprintf("Der Rheinpegel ist derzeit %.2f m, das sind %d Kölschstangen #koeln #rhein", cm_to_m(level_history[0]), convert_to_koelsch(level_history[0])))
 }
 
@@ -213,6 +239,7 @@ func check(e error) {
 	}
 }
 
+// tweet sends a Tweet
 func tweet(tweet_text string) {
 	// twitter api
 	anaconda.SetConsumerKey(configuration.Twitter_consumer_key)
@@ -260,13 +287,21 @@ func main() {
 		cur_tendency = find_tendency()
 		logger.Printf("Tendenz ist %s (war %s), Varianz ist %d, Hour ist %d, Intervall ist %d Stunden", cur_tendency, old_tendency, variance, now.Hour(), configuration.Tweet_after_x_hours)
 
-		// Streungslimit ueberschritten?
+		// variance above limit?
 		if variance > int64(configuration.Min_change_cm) {
 			logger.Printf("Varianz %d ist größer als Limit %d", variance, configuration.Min_change_cm)
 			write_tendency_tweet(cur_tendency)
 		} else if int64(tweet_date_lower_unix) > last_tweet_timestamp {
-			// wiedermal Zeit fuer einen Tweet
-			write_scheduled_tweet()
+			// scheduled tweet
+
+			// init random generator
+			rand.Seed(time.Now().UnixNano())
+			i := rand.Intn(100)
+			if i < 20 {
+				write_scheduled_koelsch_tweet()
+			} else {
+				write_scheduled_message_tweet(cur_tendency)
+			}
 		}
 		sleep_hours := configuration.Sleep_time_in_hours
 		logger.Printf("Will go to sleep for %d hours..", sleep_hours)
